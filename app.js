975fb6632b1a1c57d4afc42d0a5439cf8eeaa890
Bikesh <iambikeshkumar07@gmail.com>
2024-11-04 00:00:47 +0530

    completed lec-1

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("bikesh")
+const accountId = 14445553;
+let accountEmail = "bikeshKumar@gmail.com";
+var accountPassword = "12345";
+accountCity = "Bihar";
+let accountState;
+
+
+// accountId=2; not allowed 
+accountEmail="Bikeshsharma@gmail.com"
+accountPassword="78945"
+accountCity="Patna"
+
+console.log(accountId);
+
+/*
+prefere not to use var
+because of 
+
+*/
+
+console.table([accountId, accountEmail, accountCity,accountPassword,accountState])
