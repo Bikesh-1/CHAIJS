7b2fb5296573ee7d4abd5a73c3cc41b8dfaeedc8
Bikesh <iambikeshkumar07@gmail.com>
2024-11-04 00:21:59 +0530

    lecture 2 done

---
+++ chai_lec-2.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using node js node brower
+
+
+let name = "bikesh";
+let age = 18;
+let isLoggedIn = false;
+
+// number => 2 to power 53
+//bigInt
+//string => double quotes
+//boolean => true/false
+//null => standlone value 
+//undefined => variable is declared but not assigned any value
+
+//symbol  => unique value
+
+console.log(typeof null) // null object hai 
+
+
