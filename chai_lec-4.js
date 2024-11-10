f28cbd4fd46c4c75a5c9e9e2f5b2aaf899b8c2e7
Bikesh <iambikeshkumar07@gmail.com>
2024-11-10 12:07:25 +0530

    operation complete

---
+++ chai_lec-4.js
@@ -0,0 +1,29 @@
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " bikesh"
+
+let str3 = str1 + str2;
+
+// console.log(str3)
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true)
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
