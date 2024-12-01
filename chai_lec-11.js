152851571708218d2366e8542f81c800b3544ee7
Bikesh <iambikeshkumar07@gmail.com>
2024-12-02 01:30:01 +0530

    class done

---
+++ chai_lec-11.js
@@ -0,0 +1,24 @@
+//array
+const myArr=[0,1,2,3,4,5,6,7];
+// console.log(myArr[4]);
+
+//array methods
+
+// myArr.push(6);
+// myArr.pop();
+
+// myArr.unshift(9);
+
+// myArr.shift();
+
+
+// const newArr = myArr.join();
+// console.log(myArr);
+// console.log(typeof newArr);
+
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
